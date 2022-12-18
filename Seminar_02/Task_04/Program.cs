03304bca7cc73ba93b3d84f9f4b65c9c5a793dff
SMV <kraft088@gmail.com>
2022-12-18 17:58:44 +1100

    Add Task 4

---
+++ Seminar_02/Task_04/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Напишите программу, которая принимает на вход число и проверяет,
+кратно ли оно одновременно 7 и 23.
+14 -> нет
+46 -> нет
+161 -> да
+*/
+Console.WriteLine ("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+bool remains = (a % 7 == 0) && (a % 23 == 0);
+if (remains)
+{
+   Console.WriteLine ("Да") ;
+}
+else
+{
+   Console.WriteLine ("Нет");
+}
+
