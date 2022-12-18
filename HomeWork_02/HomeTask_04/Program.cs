1bd6ec1a52700e18c1c3ebdcbedff00ed56f6098
SMV <kraft088@gmail.com>
2022-12-18 20:06:57 +1100

    Add Home Task 4

---
+++ HomeWork_02/HomeTask_04/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, 
+является ли этот день выходным.
+6 -> Да
+7 -> Да
+1 -> Нет
+*/
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+
+string[] days = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+
+int value = Prompt("Введите номер дня недели > ");
+if(value == 6 || value == 7) 
+{
+    Console.WriteLine("Да");
+} 
+else
+{
+    Console.WriteLine("Нет");
+}
