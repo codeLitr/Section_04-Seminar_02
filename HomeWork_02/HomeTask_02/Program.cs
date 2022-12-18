0ab654c3deca79f4bfb96c29bbf21673c2645d12
SMV <kraft088@gmail.com>
2022-12-18 20:05:05 +1100

    Add Home Task 2

---
+++ HomeWork_02/HomeTask_02/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру это числа.
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+int value = new Random().Next(100, 999);
+Console.WriteLine(value);
+
+string str = value.ToString();
+    int[] remoteNumber = new int[str.Length];
+    for (int i = 0; i < str.Length; i++)
+    {
+    remoteNumber[i] = int.Parse(str[i].ToString());
+    }
+Console.Write(remoteNumber[0]);
+Console.Write(remoteNumber[2]);
+
